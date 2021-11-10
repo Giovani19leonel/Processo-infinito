cdfbd7972823ca6e1d31ec72be8b7b434d8d2c99
Giovani Henrique <giovani142002@hotmail.com>
2021-11-10 18:36:20 -0200

    Adicionar arquivos de projeto.

---
+++ scripts/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace scripts
+{
+   
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Cria uma variavel de um processo nullo.
+            Process ArqProcesso = null;
+            // Inicia o processo desejado.
+            ArqProcesso = Process.Start("mspaint.exe");
+            // Ponto de Inicio da analise
+            REVISAO:
+            // Faz uma atualização do estado atual do processo, se ele foi fechado ou não.
+            ArqProcesso.Refresh();  // Important
+            if (ArqProcesso.HasExited)
+            {
+                // Entra no if se o processo foi fechado, então reinicia o processo e atualiza denovo;
+                ArqProcesso = Process.Start("mspaint.exe");
+                ArqProcesso.Refresh();
+                goto REVISAO;
+            }
+            else
+            {
+                goto REVISAO;
+            }
+        }
+    }
+    }
+
